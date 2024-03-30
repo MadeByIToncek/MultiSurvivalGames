92457530440380d87fb1cb51cd4ae5d91d44627d
Made By IToncek <toncek2019@post.cz>
2024-03-30 21:53:14 +0100

    Init

--- src/main/java/space/itoncek/msg/commands/StartRoundCommand.java
+++ src/main/java/space/itoncek/msg/commands/StartRoundCommand.java
@@ -1,2 +1,16 @@
-package space.itoncek.msg.commands;public class StartRoundCommand {
+package space.itoncek.msg.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.*;
+
+public class StartRoundCommand implements CommandExecutor {
+	@Override
+	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+		if (sender.hasPermission("msg.command.startround") || sender.isOp()) {
+
+		}
+		return true;
+	}
 }
--- src/main/java/space/itoncek/msg/database/DBMGR.java
+++ src/main/java/space/itoncek/msg/database/DBMGR.java
@@ -1,2 +1,15 @@
-package space.itoncek.msg.database;public class DBMGR {
+package space.itoncek.msg.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBMGR {
+	private final Connection conn;
+	public DBMGR(String host, int port, String database, String username, String password) throws SQLException {
+		conn = DriverManager.getConnection("jdbc:mysql://%s:%d/%s".formatted(host,port,database), username, password);
+	}
+	public void migrate() {
+
+	}
 }
